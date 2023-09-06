545eff5ef5d03d944d0da846382b6c887601b750
Joseph Rafael Ferrer <rafael2x0@gmail.com>
2023-09-06 21:17:18 +0800

    Firing angle estimation test

--- src/trajectory.rs
+++ src/trajectory.rs
@@ -197,6 +197,11 @@ mod tests {
 
     const EPSILON: FloatType = 5e-2;
 
+    const KG_PER_LB: FloatType = 0.45359237;
+    const M_PER_IN: FloatType = 0.0254;
+    const PASCALS_PER_MILLIBAR: FloatType = 100.0;
+    const KELVIN_OFFSET: FloatType = 273.15;
+
     // Example 8.1 of Modern Exterior Ballistics
     #[test]
     fn test_vacuum_trajectory() {
@@ -297,11 +302,6 @@ mod tests {
 
     #[test]
     fn test_flat_fire_trajectory_no_wind() {
-        const KG_PER_LB: FloatType = 0.45359237;
-        const M_PER_IN: FloatType = 0.0254;
-        const PASCALS_PER_MILLIBAR: FloatType = 100.0;
-        const KELVIN_OFFSET: FloatType = 273.15;
-
         // Validation data using JBM Ballistics calc
         // https://www.jbmballistics.com/cgi-bin/jbmtraj-5.1.cgi
         let reference: &[(FloatType, FloatType, FloatType, FloatType, FloatType)] = &[
@@ -344,7 +344,7 @@ mod tests {
 
         let drag_func = create_standard_drag_function(StandardDragFunction::G1, bc);
 
-        let ranges: Vec<_> = reference.iter().map(|x| x.0 as FloatType).collect();
+        let ranges: Vec<_> = reference.iter().map(|x| x.0).collect();
 
         let trajectory = calc_trajectory_on_range_intervals(
             x0, v0, drag_func, wind, temp, pressure, rh, elevation, dt, t_max, ranges,
@@ -374,11 +374,6 @@ mod tests {
 
     #[test]
     fn test_flat_fire_trajectory_strong_wind() {
-        const KG_PER_LB: FloatType = 0.45359237;
-        const M_PER_IN: FloatType = 0.0254;
-        const PASCALS_PER_MILLIBAR: FloatType = 100.0;
-        const KELVIN_OFFSET: FloatType = 273.15;
-
         // Validation data using JBM Ballistics calc
         // https://www.jbmballistics.com/cgi-bin/jbmtraj-5.1.cgi
         let reference: &[(
@@ -430,7 +425,122 @@ mod tests {
 
         let drag_func = create_standard_drag_function(StandardDragFunction::G1, bc);
 
-        let ranges: Vec<_> = reference.iter().map(|x| x.0 as FloatType).collect();
+        let ranges: Vec<_> = reference.iter().map(|x| x.0).collect();
+
+        let trajectory = calc_trajectory_on_range_intervals(
+            x0, v0, drag_func, wind, temp, pressure, rh, elevation, dt, t_max, ranges,
+        );
+
+        assert_eq!(reference.len(), trajectory.len());
+
+        for ((state, t), ref_data) in trajectory.iter().zip(reference) {
+            let x = state.pos.x.round();
+            let z = (state.pos.z / M_PER_IN * 10.0).round() / 10.0;
+            let y = (state.pos.y / M_PER_IN * 10.0).round() / 10.0;
+            let speed = (state.vel.length() * 10.0).round() / 10.0;
+            let t = (t * 1e3).round() / 1e3;
+
+            let drop_moa = if state.pos.x == 0.0 {
+                0.0
+            } else {
+                ((state.pos.z / state.pos.x).atan().to_arcminute() * 10.0).round() / 10.0
+            };
+
+            let windage_moa = if state.pos.x == 0.0 {
+                0.0
+            } else {
+                ((state.pos.y / state.pos.x).atan().to_arcminute() * 10.0).round() / 10.0
+            };
+
+            assert!(almost_equal(x, ref_data.0, EPSILON));
+            assert!(almost_equal(z, ref_data.1, EPSILON));
+            assert!(almost_equal(drop_moa, ref_data.2, EPSILON));
+            assert!(almost_equal(y, ref_data.3, EPSILON));
+            assert!(almost_equal(windage_moa, ref_data.4, EPSILON));
+            assert!(almost_equal(speed, ref_data.5, EPSILON));
+            assert!(almost_equal(t, ref_data.6, EPSILON));
+        }
+    }
+
+    #[test]
+    fn test_firing_angle_estimation_from_zeroing() {
+        let reference: &[(
+            FloatType,
+            FloatType,
+            FloatType,
+            FloatType,
+            FloatType,
+            FloatType,
+            FloatType,
+        )] = &[
+            (0.0, -1.5, 0.0, 0.0, 0.0, 1000.0, 0.000),
+            (100.0, 1.5, 1.3, -0.7, -0.6, 933.3, 0.104),
+            (200.0, -0.0, -0.0, 0.0, 0.0, 869.9, 0.215),
+            (300.0, -6.6, -1.9, 2.4, 0.7, 809.2, 0.334),
+            (400.0, -19.1, -4.2, 6.5, 1.4, 751.2, 0.462),
+            (500.0, -38.4, -6.7, 12.6, 2.2, 695.6, 0.600),
+            (600.0, -65.8, -9.6, 20.9, 3.0, 642.3, 0.750),
+            (700.0, -102.5, -12.8, 31.8, 4.0, 591.5, 0.912),
+            (800.0, -150.3, -16.4, 45.4, 5.0, 543.3, 1.089),
+            (900.0, -211.2, -20.5, 62.1, 6.0, 498.1, 1.281),
+            (1000.0, -287.8, -25.1, 82.3, 7.2, 456.4, 1.491),
+            (1100.0, -382.9, -30.4, 106.3, 8.4, 418.7, 1.720),
+            (1200.0, -500.1, -36.4, 134.2, 9.8, 385.7, 1.969),
+            (1300.0, -643.3, -43.2, 166.2, 11.2, 358.1, 2.238),
+            (1400.0, -816.6, -50.9, 201.9, 12.6, 336.1, 2.527),
+            (1500.0, -1024.2, -59.6, 241.1, 14.0, 318.8, 2.834),
+            (1600.0, -1269.9, -69.3, 283.3, 15.5, 304.9, 3.155),
+            (1700.0, -1557.3, -80.0, 328.2, 16.9, 293.1, 3.491),
+            (1800.0, -1890.0, -91.7, 375.7, 18.2, 282.9, 3.839),
+            (1900.0, -2271.3, -104.4, 425.7, 19.6, 273.7, 4.201),
+            (2000.0, -2704.6, -118.1, 478.1, 20.9, 265.4, 4.574),
+        ];
+
+        let muzzle_speed = 1000.0;
+        let bc = 0.5 * KG_PER_LB / (M_PER_IN * M_PER_IN);
+        let sight_height = 1.5 * M_PER_IN;
+        let wind_speed = 5.0; // 5 m/s to the right
+        let wind = Vector3::new(0.0, wind_speed, 0.0);
+        let temp = 25.0 + KELVIN_OFFSET; // 25 degC
+        let pressure = 1013.25 * PASCALS_PER_MILLIBAR; // 1013.25 millibars
+        let rh = 0.0;
+        let elevation = 0.0;
+        let dt = 0.01;
+        let t_max = 10.0;
+
+        let zero_range = 200.0;
+        let zero_elevation = 0.0;
+
+        let x0 = Vector3::new(0.0, 0.0, elevation - sight_height);
+
+        let drag_func = create_standard_drag_function(StandardDragFunction::G1, bc);
+
+        let (ver_angle, hor_angle) = solve_initial_velocity(
+            x0,
+            muzzle_speed,
+            drag_func,
+            zero_range,
+            zero_elevation,
+            wind,
+            temp,
+            pressure,
+            rh,
+            elevation,
+            dt,
+            t_max,
+        )
+        .unwrap();
+
+        let v0 = muzzle_speed
+            * Vector3::new(
+                ver_angle.cos() * hor_angle.cos(),
+                hor_angle.sin(),
+                ver_angle.sin() * hor_angle.cos(),
+            );
+
+        let drag_func = create_standard_drag_function(StandardDragFunction::G1, bc);
+
+        let ranges: Vec<_> = reference.iter().map(|x| x.0).collect();
 
         let trajectory = calc_trajectory_on_range_intervals(
             x0, v0, drag_func, wind, temp, pressure, rh, elevation, dt, t_max, ranges,
